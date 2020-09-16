37566c9498b7e59a252ac195ca0473fcf9dc9c09
Noah Soukup <noahsoukupraesch@gmail.com>
2020-09-16 01:16:36 -0500

    heroku?

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,8 +12,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+app.use(routes);
 // Define API routes here
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksdb");
 
 // Send every other request to the React app
 // Define any API routes before this runs
