d011a0bc13dc18423b06e40cd85918e1fec31cfe
hozerpffp <hozerpffp2009@gmail.com>
2020-09-17 18:49:07 -0500

    created pages folder with home.js file

--- client/src/App.js
+++ client/src/App.js
@@ -40,18 +40,4 @@ function App() {
   );
 }
 
-//   return (
-//     <div className="App">
-//       <div className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h2>Gourmet Reciepies </h2>
-//       </div>
-//       <p className="App-intro">
-//         To get started, edit <code>src/App.js</code> and save to reload.
-//       </p>
-//     </div>
-//   );
-// }
-
-
-// export default App;
+export default App;
