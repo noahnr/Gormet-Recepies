cd61bbc098d66e1def92bf5862cf75e309a0b988
hozerpffp <hozerpffp2009@gmail.com>
2020-09-17 20:23:56 -0500

    modified app.js, login.js and signup.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,37 +7,36 @@
 // function App() {
 //   const url =  'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}';
 //   const APP_ID = "1b660772";
-//   const YOUR_APP_KEY = 
+//   const YOUR_APP_KEY =
 //   "8b4a7a21e724774796e8f3be6a73493d";
 //   const getData =  async ()=> {
 //     const result = await Axios.get(url);
 //     console.log(result)
 //   }
-import React, { Component } from "react";
-import Login from "./components/Login"
-import SignUp from "./components/SignUp"
+import React, {Component} from "react";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Nav from "./components/Nav";
 
 // import stylesheets from './components/styles';
 
-function App() {
-  return (
-    <Router>
-    <div>
-    <Nav />
-    <Switch>
-      <Route exact path={["/", "/login"]}>
-        <Login />
-      </Route>
-      <Route exact path="/login/signup">
-        <SignUp />
-      </Route>
-    </Switch>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
