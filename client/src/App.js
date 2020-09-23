6dc057bff1df91c4e7b448342ec981d6a499f565
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-22 20:04:16 -0500

    added alert and changed recipe.info

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppWithRouterAccess from './AppWithRouterAccess';
 
+
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,7 @@ class App extends Component {
   }
 }
 
+  
 export default App;
 
 
