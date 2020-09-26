465503dfe803a7fe0b3bab6398d85fd169ffb8bc
rudy gauna <rudy.gauna@gmail.com>
2020-09-26 12:36:52 -0500

    restoring

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,5 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import {MDBBtn} from "mdbreact";
-
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 import { MDBBtn } from "mdbreact";
