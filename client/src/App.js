6f9f2916ad329d165e18e86ce5aed19d5dea6674
rudy gauna <rudy.gauna@gmail.com>
2020-09-26 13:28:47 -0500

    correction RecipeInfo.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppWithRouterAccess from './AppWithRouterAccess';
 import Contacts from "./components/Contacts"
 import Header from "./components/Header"
-import {Nav} from "./styled/nav"
-import Carousel from "./components/Carousel"
-import { Container } from './styled/container';
 
 
 
@@ -13,12 +10,8 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Nav/>
-        <Container>
         <Header />
-        <Carousel/>
         <AppWithRouterAccess />
-        </Container>
         <Contacts />
       </Router>
     );
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,6 +2,8 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
+import {MDBBtn} from "mdbreact";
+
 
 function Nav() {
   return (
